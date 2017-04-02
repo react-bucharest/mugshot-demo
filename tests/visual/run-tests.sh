5956f22d7d5fc91abd2c7cf46834604d6f45642f
Valentin Radulescu <radulescu.valentin@gmail.com>
2017-04-02 13:26:57 +0300

    Run tests in travis #1

--- tests/visual/run-tests.sh
+++ tests/visual/run-tests.sh
@@ -4,5 +4,11 @@ docker-compose build
 docker-compose up -d selenium
 docker-compose up tests
 
+RESULT=$(docker-compose ps -q \
+  | xargs docker inspect -f '{{ .State.ExitCode }}' \
+  | grep -v 0 | wc -l | tr -d ' ')
+
 docker-compose stop
 docker-compose rm -vf
+
+exit $RESULT
