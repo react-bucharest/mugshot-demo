ed52de4d1117dbe73c779fad2430ab9c359d1322
Valentin Radulescu <radulescu.valentin@gmail.com>
2017-06-04 14:48:50 +0300

    Update fixtures path for visual tests

--- tests/visual/setup.js
+++ tests/visual/setup.js
@@ -5,7 +5,7 @@ const glob = require('glob');
 
 const PLAYGROUND_URL = 'http://playground:8989/';
 const COMPONENTS_DIR = filePaths.src;
-const COMPONENTS_PATTERN = path.join('*', 'fixtures', '**.js');
+const COMPONENTS_PATTERN = path.join('*', '__fixtures__', '**.js');
 
 exports.getUrls = function(ignored) {
   const components = _getComponents(COMPONENTS_DIR, ignored);
